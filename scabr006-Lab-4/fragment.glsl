d56e51b08381307fc4ebe9186491e2dfbe52b04f
Stephanie Cabrera <scabr006@ucr.edu>
2020-02-06 14:18:09 -0800

    Added recent Lab3 and new lab4 files

---
+++ scabr006-Lab-4/fragment.glsl
@@ -0,0 +1,11 @@
+varying vec4 position;
+varying vec3 normal;
+varying vec3 light_direction;
+
+void main()
+{
+    vec4 ambient = vec4(1, 0, 0, 1);
+    vec4 diffuse = vec4(0, 1, 0, 1);
+    vec4 specular = vec4(0, 0, 1, 1);
+    gl_FragColor = ambient + diffuse + specular;
+}
